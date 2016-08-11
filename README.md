0d1c402d50a6d32530966a03477a1ace66d79bb6
Jose Constela <jose@joseconstela.com>
2016-08-11 19:20:33 +0200

    Docs

--- README.md
+++ README.md
@@ -22,7 +22,23 @@ The fileInfo parameter have the following structure:
   ext: '.PDF',
   name: 'myPdfFile',
   route: '/.../files-analysis-starter-kit/data/subfolder/myPdfFile.PDF',
-  mime: 'application/pdf'
+  mime: 'application/pdf',
+  stats: {
+    dev: 16777220,
+    mode: 33188,
+    nlink: 1,
+    uid: 501, // user-id of the owner of the file.
+    gid: 20, // group-id of the owner of the file.
+    rdev: 0,
+    blksize: 4096,
+    ino: 29670994,
+    size: 68704, // Size in bytes
+    blocks: 136,
+    atime: Thu Aug 11 2016 13:30:51 GMT+0200 (CEST), // last access
+    mtime: Tue Apr 01 2008 08:52:56 GMT+0200 (CEST), // last modification
+    ctime: Tue Aug 09 2016 04:20:35 GMT+0200 (CEST), // creation
+    birthtime: Tue Apr 01 2008 08:52:56 GMT+0200 (CEST)
+  }
 }
 ```
 
--- libs/analysis.js
+++ libs/analysis.js
@@ -6,18 +6,37 @@
  * @param  {Object}   fileInfo Contains the file information
  *
  * Example of fileInfo parameter:
- * { root: '/',
- * dir: '/.../files-analysis-starter-kit/data/L11',
- * base: 'myPdfFile.PDF',
- * ext: '.PDF',
- * name: 'myPdfFile',
- * route: '/.../files-analysis-starter-kit/data/L11/myPdfFile.PDF',
- * mime: 'application/pdf' }
+ * {
+ *   root: '/',
+ *   dir: '/.../files-analysis-starter-kit/data/subfolder',
+ *   base: 'myPdfFile.PDF',
+ *   ext: '.PDF',
+ *   name: 'myPdfFile',
+ *   route: '/.../files-analysis-starter-kit/data/subfolder/myPdfFile.PDF',
+ *   mime: 'application/pdf',
+ *   stats: {
+ *     dev: 16777220,
+ *     mode: 33188,
+ *     nlink: 1,
+ *     uid: 501,                  // user-id of the owner of the file.
+ *     gid: 20,                   // group-id of the owner of the file.
+ *     rdev: 0,
+ *     blksize: 4096,
+ *     ino: 29670994,
+ *     size: 68704,               // Size in bytes
+ *     blocks: 136,
+ *     atime: Thu Aug 11 2016 13:30:51 GMT+0200 (CEST),     // last access
+ *     mtime: Tue Apr 01 2008 08:52:56 GMT+0200 (CEST),     // last modification
+ *     ctime: Tue Aug 09 2016 04:20:35 GMT+0200 (CEST),     // creation
+ *     birthtime: Tue Apr 01 2008 08:52:56 GMT+0200 (CEST)
+ *   }
+ * }
  *
  * @param  {Function} callback [description]
  * @return {[type]}            [description]
  */
 const work = (dbs, fileInfo, callback) => {
+  console.log(fileInfo)
   setImmediate(callback)
 }
 
