87402b435f59270e9c2a8d8e219721ab5f3fa99b
Jose Constela <jose@joseconstela.com>
2016-08-11 18:38:56 +0200

    Add docs

--- libs/analysis.js
+++ libs/analysis.js
@@ -1,5 +1,22 @@
 'use strict'
 
+/**
+ * Do the actual analysis
+ * @param  {Object}   dbs      Contains DB connections
+ * @param  {Object}   task     Contains the file information
+ *
+ * Example of task parameter:
+ * { root: '/',
+ * dir: '/.../files-analysis-starter-kit/data/L11',
+ * base: 'myPdfFile.PDF',
+ * ext: '.PDF',
+ * name: 'myPdfFile',
+ * route: '/.../files-analysis-starter-kit/data/L11/myPdfFile.PDF',
+ * mime: 'application/pdf' }
+ *
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
 const work = (dbs, task, callback) => {
   setImmediate(callback)
 }
